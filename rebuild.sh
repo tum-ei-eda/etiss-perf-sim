b6b71b82ffaee5bcdf0c25ceb1120a04eb8fe130
Conrad Foik <ge54huz@foik.eda.cit.tum.de>
2026-06-06 13:08:26 +0200

    Commit for test-version origin.

--- rebuild.sh
+++ rebuild.sh
@@ -5,6 +5,9 @@ set -e
 . $(dirname "${0}")/.env
 
 echo " > Re-Installing ETISS"
+start=$(date +%s%N)
 cd ${EPS_ETISS_BUILD}
 cmake -DCMAKE_BUILD_TYPE=Release -DETISS_BUILD_MANUAL_DOC=ON -DCMAKE_INSTALL_PREFIX:PATH=${EPS_ETISS_INSTALLED} ..
 make -j$(nproc) install
+end=$(date +%s%N)
+echo "Done rebuilding ETISS-PerfSim. Time spend: $(( (end - start)/1000000 )) ms"
